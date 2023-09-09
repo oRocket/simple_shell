a241b25d0bf0a7b6eaf62939061c3330df1a72b5
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-09-09 10:09:40 -0700

    little progress

--- shell.h
+++ shell.h
@@ -7,8 +7,16 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 #define MAX_INPUT_SIZE 1024
+#define BUFFER_SIZE 1024
+
+/* Define a static buffer for reading input */
+static char input_buffer[MAX_INPUT_SIZE];
+static size_t buffer_position = 0;  /* Position within the buffer */
+
+
 
 extern char **environ;
 
@@ -20,6 +28,7 @@ typedef struct {
 void display_prompt(void);
 void execute_command(Command *cmd);
 void parse_input(char *input, Command *cmd);
-
+ssize_t my_getline(char **lineptr, size_t *n, FILE *stream);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 
 #endif /* shell.h */

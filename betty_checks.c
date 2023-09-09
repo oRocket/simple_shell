ca3b10bf07863888439ad7ec10627b6f03bb590b
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-09-09 04:28:02 -0700

    Proud Betty

---
+++ betty_checks.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Authors: Albert and Benedict
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int z;
+
+	for (z = 0; z < 15; z++) /* A for loop to print a message 15 times */
+	{
+		printf("Hello, Albert and Benedict!\n");
+	}
+	return (0);
+}

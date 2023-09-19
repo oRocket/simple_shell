349660f41985a638ff220ea2f8e9d0204afa4611
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-09-19 00:18:45 -0700

    shell

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,43 +7,28 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdbool.h>
-#include <limits.h> /* PATH_MAX */
-#include <errno.h>  /* errno */
+#include <errno.h>
 
-#define MAX_INPUT_LENGTH 1024
-#define MAX_ARGS 64
 #define MAX_INPUT_SIZE 1024
-extern size_t buffer_position;
-extern char input_buffer[MAX_INPUT_SIZE];
+#define BUFFER_SIZE 1024
 
-/* Struct to hold shell state */
+/* Define a static buffer for reading input */
+static char input_buffer[MAX_INPUT_SIZE];
+static size_t buffer_position = 0;  /* Position within the buffer */
+
+
+
+extern char **environ;
+
 typedef struct {
-	char current_dir[PATH_MAX];
-	int exit_status;
-} ShellState;
+	char *command;
+	char *args[MAX_INPUT_SIZE / 2];
+} Command;
 
-/* Custom function to split input into arguments */
-void split_input(const char *input, char *args[], int *arg_count);
-
-/* Function to initialize the shell state */
-void init_shell(ShellState *shell);
-
-/* Function to handle the "cd" command */
-void cd_command(ShellState *shell, char *args[], int arg_count);
-
-/* Function to execute external commands */
-void execute_command(ShellState *shell, char *args[]);
-
-void handle_unsetenv(char *args[], int arg_count);
-void handle_setenv(char *args[], int arg_count);
-void handle_exit(ShellState *shell, char *args[], int arg_count);
-void print_exit_msg(int status);
-void run_shell(void);
-void print_usage(const char *program_name);
-
+void display_prompt(void);
+void execute_command(Command *cmd);
+void parse_input(char *input, Command *cmd);
 ssize_t my_getline(char **lineptr, size_t *n, FILE *stream);
-size_t my_strlen(const char *str);
-int my_strcmp(const char *str1, const char *str2);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 
 #endif /* shell.h */

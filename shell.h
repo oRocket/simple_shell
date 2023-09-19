144f3ee19e793cdfa46a251ff41a757707de70d3
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-09-19 00:36:16 -0700

    update

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,28 +7,38 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <errno.h>
+#include <stdbool.h>
+#include <limits.h> /* PATH_MAX */
+#include <errno.h>  /* errno */
 
+#define MAX_INPUT_LENGTH 1024
+#define MAX_ARGS 64
 #define MAX_INPUT_SIZE 1024
-#define BUFFER_SIZE 1024
-
-/* Define a static buffer for reading input */
-static char input_buffer[MAX_INPUT_SIZE];
-static size_t buffer_position = 0;  /* Position within the buffer */
+extern size_t buffer_position;
+extern char input_buffer[MAX_INPUT_SIZE];
 
 
-
-extern char **environ;
-
+/* Struct to hold shell state */
 typedef struct {
-	char *command;
-	char *args[MAX_INPUT_SIZE / 2];
-} Command;
+    char current_dir[PATH_MAX];
+    int exit_status;
+} ShellState;
 
-void display_prompt(void);
-void execute_command(Command *cmd);
-void parse_input(char *input, Command *cmd);
+/* Custom function to split input into arguments */
+void split_input(const char *input, char *args[], int *arg_count);
+
+/* Function to initialize the shell state */
+void init_shell(ShellState *shell);
+
+/* Function to handle the "cd" command */
+void cd_command(ShellState *shell, char *args[], int arg_count);
+
+/* Function to execute external commands */
+void execute_command(ShellState *shell, char *args[]);
+
 ssize_t my_getline(char **lineptr, size_t *n, FILE *stream);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+size_t my_strlen(const char *str);
+int my_strcmp(const char *str1, const char *str2);
+int my_execvp(const char *file, char *const argv[]);
 
 #endif /* shell.h */

e3389d97b2440549881e0f7251d205c7286fbdc1
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-09-19 08:37:41 -0700

    hmm

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_LENGTH 1024
+#define MAX_ARGS 64
+
 /**
  * main - Entry point for the simple shell.
  *
@@ -8,25 +17,29 @@
 int main(void)
 {
 	int arg_count = 0;
-	char input[MAX_INPUT_LENGTH], *token, *args[MAX_ARGS];
+	char input[MAX_INPUT_LENGTH], *token;
+	char* argv[] = {"/bin/ls", "/bin/ls -l"}
+	char* envp[] = { "some", "environment", NULL };
 
 	while (1)
 	{
-		write(STDOUT_FILENO, "$ ", 2);
+		printf("$ ");
 		fflush(stdout);
 
 		/* Read user input */
 		if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
 		{
 			/* Handle Ctrl+D (end of file) */
-			write(STDOUT_FILENO, "\nExiting shell.\n", 16);
+			printf("\nExiting shell.\n");
 			exit(0);
 		}
 
 		input[strlen(input) - 1] = '\0'; /* Remove the newline character */
 
+		char *args[MAX_ARGS];
+
 		/* Tokenize input into command and arguments */
-		token = strtok(input, " ");
+		*token = strtok(input, " ");
 		while (token != NULL)
 		{
 			args[arg_count++] = token;
@@ -40,15 +53,12 @@ int main(void)
 		}
 
 		/* Check if the command exists in PATH and execute it */
-		if (execvp(args[0], args) == -1)
+		if (execve(argv, envp) == -1)
 		{
 			perror("exec");
-			write(STDOUT_FILENO, "Command not found: ", 19);
-			write(STDOUT_FILENO, args[0], strlen(args[0]));
-			write(STDOUT_FILENO, "\n", 1);
+			printf("Command not found: %s\n", args[0]);
 		}
 	}
 
 	return (0);
 }
-
